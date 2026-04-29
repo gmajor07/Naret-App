954b475299bbe949d6cdd01a756a2660c0ec8914
“gmajor07” <gkyando5@gmail.com>
2026-04-29 13:40:45 +0300

    favicon

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,8 +2,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>Naret Company</title>
-<link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}?v={{ filemtime(public_path('favicon.svg')) }}">
-<link rel="shortcut icon" href="{{ asset('favicon.svg') }}?v={{ filemtime(public_path('favicon.svg')) }}">
+<link rel="icon" type="image/svg+xml" href="{{ asset('assets/img/favicon.svg') }}?v={{ filemtime(public_path('assets/img/favicon.svg')) }}">
+<link rel="shortcut icon" type="image/svg+xml" href="{{ asset('assets/img/favicon.svg') }}?v={{ filemtime(public_path('assets/img/favicon.svg')) }}">
 <!-- Font Awesome -->
 <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
 <!-- Ionicons -->
